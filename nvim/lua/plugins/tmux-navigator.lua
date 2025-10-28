5f7a727e47b58b5f46f4a0501224fde1c5280e19
Raphael Cassio de Souza <rapha.cassio@gmail.com>
2025-10-28 14:43:10 -0700

    Add vim-tmux-navigator plugin for seamless navigation
    Enables seamless navigation between vim splits and tmux panes using
    Ctrl+h/j/k/l without needing to think about boundaries.
    
    Features:
    - Bidirectional navigation (vim ↔ tmux)
    - Automatic boundary detection
    - Lazy-loaded on command or keybinding
    - Works with tmux plugin (christoomey/vim-tmux-navigator)
    
    Keybindings:
    - Ctrl+h/j/k/l - Navigate splits/panes
    - Ctrl+\ - Navigate to previous split/pane

---
+++ nvim/lua/plugins/tmux-navigator.lua
@@ -0,0 +1,21 @@
+-- Filename: tmux-navigator.lua
+-- Last change: Sat, 25 Oct 2025, 6:29PM
+
+return {
+  "christoomey/vim-tmux-navigator",
+  cmd = {
+    "TmuxNavigateLeft",
+    "TmuxNavigateDown",
+    "TmuxNavigateUp",
+    "TmuxNavigateRight",
+    "TmuxNavigatePrevious",
+    "TmuxNavigatorProcessList",
+  },
+  keys = {
+    { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+    { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+    { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+    { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+  },
+}
